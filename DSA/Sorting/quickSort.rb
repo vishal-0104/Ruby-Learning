549f6688ed582c1ea9ec4a19f79ad3f9aec84ebf
vishal2110991544 <vishal1544.be21@chitkara.edu.in>
2024-12-22 18:16:36 +0530

    [vishal]Add : added examples based on quick sort and update the quick sort file

--- DSA/Sorting/quickSort.rb
+++ DSA/Sorting/quickSort.rb
@@ -9,3 +9,31 @@ end
 
 array = [4, 2, 7, 1, 9]
 puts quick_sort(array).inspect
+
+
+=begin
+Find the Kth Largest Element in an Array
+Find the Kth largest element in an unsorted array.
+Hint: Use Quickselect or Sorting.
+=end
+
+
+def quickSort(arr, k)
+  return nil if arr.empty? || k > arr.length || k <= 0
+
+  pivot = arr.delete_at(arr.length / 2)
+  left, right = arr.partition { |x| x > pivot }
+
+  if left.length == k - 1
+    pivot
+  elsif left.length >= k
+    quickSort(left, k)
+  else
+    quickSort(right, k - left.length - 1)
+  end
+end
+
+
+array1 = [4, 2, 7, 1, 9]
+k = 2
+puts quickSort(array1, k)
