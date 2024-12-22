5abfce319ba0ca5d0f3690eb4748982256058451
vishal2110991544 <vishal1544.be21@chitkara.edu.in>
2024-12-22 18:28:37 +0530

    [vishal]Add : added examples based on merge sort and update the merge sort file

--- DSA/Sorting/mergeSort.rb
+++ DSA/Sorting/mergeSort.rb
@@ -26,3 +26,32 @@ end
 
 array = [4, 2, 7, 1, 9]
 puts merge_sort(array).inspect
+
+
+=begin
+Merge Two Sorted Arrays
+Given two sorted arrays, merge them into one sorted array without using extra space.
+Hint: Use the two-pointer technique.
+=end
+
+
+def merge_sorted_arrays(arr1, arr2)
+  m, n = arr1.length, arr2.length
+  i, j = 0, 0
+
+  while i < m
+    if arr1[i] > arr2[j]
+      
+      arr1[i], arr2[j] = arr2[j], arr1[i]
+      
+      arr2.sort!
+    end
+    i += 1
+  end
+  arr1 + arr2
+end
+
+
+arr1 = [1, 3, 5]
+arr2 = [2, 4, 6]
+puts merge_sorted_arrays(arr1, arr2).inspect
