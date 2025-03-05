834c59ac3b8882fbc87b095ac7890b0cd4c2ab08
vishal2110991544 <vishal1544.be21@chitkara.edu.in>
2025-03-05 14:28:19 +0530

    [vishal]Add: done with the turbo link function in my post_app for create delete and update posts

--- post_app/app/controllers/posts_controller.rb
+++ post_app/app/controllers/posts_controller.rb
@@ -1,70 +1,66 @@
+# app/controllers/posts_controller.rb
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[ show edit update destroy ]
+  include ActionView::RecordIdentifier
+  before_action :set_post, only: %i[show edit update destroy]
 
-  # GET /posts or /posts.json
   def index
     @posts = Post.all
   end
-
-  # GET /posts/1 or /posts/1.json
-  def show
-  end
-
-  # GET /posts/new
+   
   def new
     @post = Post.new
   end
-
-  # GET /posts/1/edit
-  def edit
-  end
-
-  # POST /posts or /posts.json
+  
   def create
     @post = Post.new(post_params)
-
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.append('posts', partial: 'posts/post', locals: { post: @post }),
+            turbo_stream.replace('post_form', partial: 'posts/form', locals: { post: Post.new })
+          ]
+        end
+        format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
       else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace('post_form', partial: 'posts/form', locals: { post: @post }) }
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /posts/1 or /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: "Post was successfully updated." }
-        format.json { render :show, status: :ok, location: @post }
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.replace(@post, partial: 'posts/post', locals: { post: @post }),
+            turbo_stream.update('post_form', '')
+          ]
+        end
+        format.html { redirect_to posts_path, notice: 'Post was successfully updated.' }
       else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace('post_form', partial: 'posts/form', locals: { post: @post }) }
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /posts/1 or /posts/1.json
   def destroy
-    @post.destroy!
-
+    @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_path, status: :see_other, notice: "Post was successfully destroyed." }
-      format.json { head :no_content }
+      format.turbo_stream { render turbo_stream: turbo_stream.remove(dom_id(@post)) }
+    format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.expect(post: [ :title, :content ])
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
